e980c2a5f6222f8c583dbb99635527a6be971439
ipkozyrin <ipkozyrin@gmail.com>
2016-06-17 16:21:14 +1000

    add generate request for get

--- src/Request.js
+++ src/Request.js
@@ -1,37 +1,54 @@
+import _ from 'lodash';
 
 export default class Request {
   constructor(main) {
     this._main = main;
   }
-  
+
   generate(type, fullPath, data) {
     var documentParams = this._main.schemaManager.getDocument(fullPath);
-    var request = {
+    var preRequest = {
       type,
       fullPath,
       data,
+      documentParams,
     };
 
-    if (documentParams) {
-      // If we want set all document
-      let document = request.data;
+    return this['_generateFor_' + preRequest.type](preRequest);
+  }
 
-      // If we want set one value to document
-      let pathToValue = _.trim(request.fullPath.split(documentParams.pathToDocument)[1], '.');
-      if (pathToValue)
-        document = _.set({}, pathToValue, request.data);
+  _generateFor_get(request) {
+    if (!request.documentParams) return request;
 
-      request = {
-        ...request,
-        pathToValue,
-        documentParams,
-        document,
-        pathToDocument: documentParams.pathToDocument,
-      };
-    }
+    let pathToField = this._getPathToField(request);
 
-    console.log(request)
+    return {
+      ...request,
+      pathToField,
+      pathToDocument: request.documentParams.pathToDocument,
+    };
+  }
 
-    return request;
+  _generateFor_set(request) {
+    if (!request.documentParams) return request;
+
+    // If we want set all the document
+    let document = request.data;
+
+    // If we want set one value to document
+    let pathToField = this._getPathToField(request);
+    if (pathToField)
+      document = _.set({}, pathToField, request.data);
+
+    return {
+      ...request,
+      pathToField,
+      document,
+      pathToDocument: request.documentParams.pathToDocument,
+    };
+  }
+
+  _getPathToField(request) {
+    return _.trim(request.fullPath.split(request.documentParams.pathToDocument)[1], '.');
   }
 }
