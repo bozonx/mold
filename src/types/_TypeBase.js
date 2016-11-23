908c63feaa54e185b5b865be34a723799d52c0e1
ivan <ipkozyrin@gmail.com>
2016-11-23 10:27:58 +1000

    doc

--- src/types/_TypeBase.js
+++ src/types/_TypeBase.js
@@ -31,20 +31,43 @@ export default class _TypeBase {
     this._mold = this._main.$$state.getMold(this._root);
   }
 
+  /**
+   * Listen changes of instance and its primitives.
+   * @param {function} handler
+   */
   onChange(handler) {
     this._main.$$state.addListener(this._root, handler);
   }
 
+  /**
+   * Listen changes of instance and its descendants.
+   * @param {function} handler
+   */
   onChangeDeep(handler) {
     this._main.$$state.addDeepListener(this._root, handler);
   }
 
+  /**
+   * Unbind event listener.
+   * @param {function} handler - handler to remove
+   */
   off(handler) {
     this._main.$$state.removeListener(this._root, handler);
   }
 
-  destroy() {
-    this._main.$$state.destroy(this._root);
+  /**
+   * It removes all the events listeners.
+   * @param {boolean} deep - remove event listeners for children deeply too.
+   */
+  destroy(deep) {
+    this._main.$$state.destroy(this._root, deep);
+  }
+
+  /**
+   * It clears mold state for current instance and for its descendants.
+   */
+  clear() {
+
   }
 
 }
