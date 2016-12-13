d283367d4dc9593240d4dfc21ee350a3e42dc511
ivan <ipkozyrin@gmail.com>
2016-12-14 09:34:10 +1000

    chore

--- src/Config.js
+++ src/Config.js
@@ -1,11 +1,23 @@
+import _ from 'lodash';
+
 export default class Config {
   constructor(conf) {
-    this._config = conf;
+    this._rawConfig = conf;
+
+    this._config = _.defaultsDeep(_.cloneDeep(this._rawConfig), this.getDefaults());
 
     // TODO: validate a config
   }
 
   get() {
     return this._config;
+  }
+
+  getDefaults() {
+    return {
+      silent: false,
+      eventEmitter: null,
+      logger: null,
+    }
   }
 }
