82ae3213e45421d13fb5931279b875baf5c6f651
ivan <ipkozyrin@gmail.com>
2017-01-28 12:16:52 +1000

    refactor

--- src/Request.js
+++ src/Request.js
@@ -9,8 +9,8 @@ export default class Request {
 
   /**
    * Send request to driver
-   * @param {string} rawRequest - method, moldPath, payload, options, metaParams
-   * @param {string} schema
+   * @param {object} rawRequest - method, moldPath, payload, options, metaParams
+   * @param {object} schema
    * @param {object|undefined} urlParams
    * @returns {Promise}
    */
@@ -29,8 +29,8 @@ export default class Request {
 
   /**
    * Send query to driver for data.
-   * @param {string} rawRequest - method, moldPath, payload, options, metaParams
-   * @param {string} schema
+   * @param {object} rawRequest - method, moldPath, payload, options, metaParams
+   * @param {object} schema
    * @param {object|undefined} urlParams
    * @returns {Promise}
    * @private
@@ -42,22 +42,28 @@ export default class Request {
     // // It rise an error if path doesn't consist with schema
     // const schema = this._main.$$schemaManager.get(schemaPath);
 
-    const req = this._generateRequest(
-      rawRequest.method,
-      rawRequest.moldPath,
-      rawRequest.payload,
-      rawRequest.options,
-      rawRequest.metaParams,
-      driverRoot,
+    const request = this._generateRequest(
+      {
+        ...rawRequest,
+        driverRoot,
+      },
       schema,
       urlParams);
-    this._main.$$log.info('---> start request: ', req);
+    this._main.$$log.info('---> start request: ', request);
 
-    return driver.startRequest(req);
+    return driver.startRequest(request);
   }
 
-  _generateRequest(method, moldPath, rawPayload, options, meta, driverRoot, schema, urlParams) {
-    let payload = rawPayload;
+  /**
+   * Generate request
+   * @param {object} rawRequest - method, moldPath, payload, options, metaParams, driverRoot
+   * @param {object} schema
+   * @param {object|undefined} urlParams
+   * @return {object}
+   * @private
+   */
+  _generateRequest(rawRequest, schema, urlParams) {
+    let payload = rawRequest.payload;
     if (_.isPlainObject(payload)) {
       payload = _.omit(_.cloneDeep(payload), ...this._main.$$config.omitParamsToRequest);
       payload = _.omitBy(payload, _.isUndefined);
@@ -67,26 +73,25 @@ export default class Request {
     // Payload can't be other type then array or plainObject
 
     const request = {
-      method,
+      ...rawRequest,
       payload,
-      moldPath,
-      driverRoot,
-      meta,
-      options,
-      url: '',
+      meta: rawRequest.metaParams,
+      // TODO: remove
+      metaParams: undefined,
+      url: undefined,
       //primaryKeyName: schema.item && findPrimary(schema.item),
     };
-    request.url = this._prepareUrl(schema.url, urlParams, request, driverRoot);
+    request.url = this._prepareUrl(schema.url, urlParams, request);
 
     // clear undefined params
     return _.omitBy(request, _.isUndefined);
   }
 
-  _prepareUrl(urlTemplate, urlTemplateParams, request, driverRoot) {
+  _prepareUrl(urlTemplate, urlTemplateParams, request) {
     if (urlTemplate) return _.template(urlTemplate)({...urlTemplateParams, request});
 
     // else generate url automatically
-    let defaultUrl = _.trimStart(request.moldPath, driverRoot);
+    let defaultUrl = _.trimStart(request.moldPath, request.driverRoot);
     defaultUrl = convertFromLodashToUrl(defaultUrl);
 
     return defaultUrl;
