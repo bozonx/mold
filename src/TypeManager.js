8db566c08ea3b0d60f6ed2ca2c8369fd189c74fc
ivan <ipkozyrin@gmail.com>
2017-11-29 12:18:51 +1000

    chore

--- src/TypeManager.js
+++ src/TypeManager.js
@@ -3,7 +3,6 @@ import _ from 'lodash';
 import ArrayType from './baseTypes/ArrayType';
 import AssocType from './baseTypes/AssocType';
 import BooleanType from './baseTypes/BooleanType';
-import CollectionType from './baseTypes/CollectionType';
 import NumberType from './baseTypes/NumberType';
 import StringType from './baseTypes/StringType';
 
@@ -15,7 +14,6 @@ export default class TypeManager {
       array: new ArrayType(),
       assoc: new AssocType(),
       boolean: new BooleanType(),
-      collection: new CollectionType(),
       number: new NumberType(),
       string: new StringType(),
     }
