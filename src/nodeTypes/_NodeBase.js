d9ef75e81f1e59a83d6c5387660fb3ce44f18adb
ivan <ipkozyrin@gmail.com>
2017-11-29 10:44:54 +1000

    refactor

--- src/nodeTypes/_NodeBase.js
+++ src/nodeTypes/_NodeBase.js
@@ -12,38 +12,24 @@ export default class _TypeBase {
    * @returns {string}
    */
   get root() {
-    return '' + this._moldPath;
+    return this._moldPath;
   }
 
   /**
    * Get real mold.
    */
   get mold() {
-    //return this._mold;
-    //return this._main.$$stateManager.getState(this._moldPath);
     return this.actions.default.mold;
-
-    //return this._mold.state;
   }
 
   get schema() {
     return this._schema;
   }
 
-  // /**
-  //  * Get copy of mold.
-  //  */
-  // getMold() {
-  //   return _.cloneDeep(this._mold);
-  // }
-
   $init(moldPath, schema) {
     this._moldPath = this._moldPath || moldPath;
     this._schema = schema;
-    // mold is just a link to the storage
-    //this._mold = this._mold || this._main.$$stateManager.getState(this._moldPath);
   }
-
 
   $createAction(actionName, cb) {
     const ActionClass = cb(_Action);
@@ -54,33 +40,24 @@ export default class _TypeBase {
     return instance;
   }
 
+  /**
+   * It clears mold state for current instance and for its descendants.
+   */
+  clear() {
+    // TODO: test it
+    return this.actions.default.clear();
+  }
 
   /**
    * Listen changes of instance and its primitives.
    * @param {function} handler
    */
   onChange(handler) {
-    // TODO: use action
-    this._main.$$state.addListener(this._storagePath, handler);
+    return this.actions.default.onChange(handler);
   }
 
   onAnyChange(handler) {
-    // TODO: use action
-    this._main.$$state.addListener(this._storagePath, handler, true);
-  }
-
-  /**
-   * Listen changes of instance and its descendants.
-   * @param {function} handler
-   */
-  onChangeDeep(handler) {
-    // TODO: use action
-    this._main.$$state.addDeepListener(this._storagePath, handler);
-  }
-
-  onAnyChangeDeep(handler) {
-    // TODO: use action
-    this._main.$$state.addDeepListener(this._storagePath, handler, true);
+    return this.actions.default.onAnyChange(handler);
   }
 
   /**
@@ -88,34 +65,37 @@ export default class _TypeBase {
    * @param {function} handler - handler to remove
    */
   off(handler) {
-    // TODO: use action
-    this._main.$$state.removeListener(this._storagePath, handler);
+    return this.actions.default.off(handler);
   }
 
   /**
    * It removes all the events listeners.
    */
   destroy() {
-    // TODO: use action
-    this._main.$$state.destroyListeners(this._storagePath);
+    return this.actions.default.destroy();
   }
 
-  /**
-   * It removes all the events listeners.
-   * Removes event listeners for children deeply too.
-   */
-  destroyDeep() {
-    // TODO: use action
-    this._main.$$state.destroyListeners(this._storagePath, true);
-  }
+  // /**
+  //  * Listen changes of instance and its descendants.
+  //  * @param {function} handler
+  //  */
+  // onChangeDeep(handler) {
+  //   // TODO: use action
+  //   this._main.$$state.addDeepListener(this._storagePath, handler);
+  // }
 
-  /**
-   * It clears mold state for current instance and for its descendants.
-   * @param {object|undefined} eventData - additional data to event
-   */
-  clear(eventData=undefined) {
-    // TODO: use action
-    this._main.$$state.clear(this._storagePath, eventData);
-  }
+  // onAnyChangeDeep(handler) {
+  //   // TODO: use action
+  //   this._main.$$state.addDeepListener(this._storagePath, handler, true);
+  // }
+
+  // /**
+  //  * It removes all the events listeners.
+  //  * Removes event listeners for children deeply too.
+  //  */
+  // destroyDeep() {
+  //   // TODO: use action
+  //   this._main.$$state.destroyListeners(this._storagePath, true);
+  // }
 
 }
