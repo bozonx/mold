3c0600dcfa9ed4cc57912d5234c3b53699d52b0c
ivan <ipkozyrin@gmail.com>
2017-12-12 11:49:38 +1000

    test

--- src/primitives/CollectionType.js
+++ src/primitives/CollectionType.js
@@ -12,9 +12,35 @@ export default class CollectionType {
     return [];
   }
 
-  validate(schema, value) {
-    return isCollection(value);
-    // TODO: do it
+  validate(schema, data) {
+    if (!isCollection(data)) return false;
+
+    const itemSchema = {
+      type: 'assoc',
+      items: schema.item,
+    };
+    let isValid = true;
+
+    _.find(data, (rawValue) => {
+      if (_.isNil(rawValue)) {
+        return;
+      }
+      else if (!_.isPlainObject(rawValue)) {
+        isValid = false;
+
+        return true;
+      }
+
+      const result = this._typeManager.validateValue(itemSchema, rawValue);
+
+      if (!result) {
+        isValid = false;
+
+        return true;
+      }
+    });
+
+    return isValid;
   }
 
   validateSchema(schema) {
@@ -22,9 +48,23 @@ export default class CollectionType {
     return true;
   }
 
-  cast(schema, rawValue) {
-    // don't cast
-    return rawValue;
+  cast(schema, data) {
+    // do nothing if there isn't schema for assoc
+    if (!schema.item) return;
+
+    const itemSchema = {
+      type: 'assoc',
+      items: schema.item,
+    };
+
+    const castedData = [];
+
+    _.each(data, (rawValue, index) => {
+      if (!_.isPlainObject(rawValue)) return;
+      castedData[index] = this._typeManager.castValue(itemSchema, rawValue);
+    });
+
+    return castedData;
   }
 
 }
