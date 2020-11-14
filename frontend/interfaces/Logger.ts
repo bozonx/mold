5e839a71afa29e7613c5b58f3c336ebee05643a6
ivan <ipkozyrin@gmal.com>
2020-11-15 08:42:31 +1000

    add

--- frontend/interfaces/Logger.ts
+++ frontend/interfaces/Logger.ts
@@ -1,3 +1,10 @@
+export type LogLevel = 'debug' | 'info' | 'warn' | 'error';
+
+export const LOG_LEVELS = ['debug', 'info', 'warn', 'error'];
+
 export interface Logger {
+  debug: (msg: string) => void;
+  info: (msg: string) => void;
+  warn: (msg: string) => void;
   error: (msg: string) => void;
 }
--- helpers/common.ts
+++ helpers/common.ts
@@ -1,8 +1,18 @@
 import {FindProps} from '../frontend/interfaces/MethodsProps';
+import {LOG_LEVELS, LogLevel} from '../frontend/interfaces/Logger';
 
 
 export function makeRequestId(props: FindProps): string {
   // TODO: use все виды props
   // TODO: из параметров сделать уникальный id
   // TODO: поля должны быть отсортированны
+}
+
+/**
+ * Makes ['info', 'warn', 'error'] if log level is 'info'
+ */
+export function calcAllowedLogLevels(logLevel: LogLevel): LogLevel[] {
+  const currentLevelIndex: number = LOG_LEVELS.indexOf(logLevel);
+
+  return LOG_LEVELS.slice(currentLevelIndex) as LogLevel[];
 }
