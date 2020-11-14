48ae686fcd438017e50885d1990c056e7b971494
ivan <ipkozyrin@gmal.com>
2020-11-14 18:37:13 +1000

    add

--- dbAdapters/PouchDbAdapter.ts
+++ dbAdapters/PouchDbAdapter.ts
@@ -1,7 +1,26 @@
 import DbAdapter from '../interfaces/DbAdapter';
+import {CreateProps, DeleteProps, GetItemProps, UpdateProps} from '../frontend/interfaces/MethodsProps';
+import {GetResult} from '../frontend/interfaces/MethodsState';
 
 
 export default class PouchDbAdapter implements DbAdapter {
   // TODO: add
-  // TODO: add to interface
+  // TODO: revuew interface
+
+  get(props: Omit<Omit<GetItemProps, 'backend'>, 'entity'>): Promise<GetResult> {
+
+  }
+
+  create(props: Omit<Omit<CreateProps, 'backend'>, 'entity'>): Promise<void> {
+
+  }
+
+  update(props: Omit<Omit<UpdateProps, 'backend'>, 'entity'>): Promise<void> {
+
+  }
+
+  delete(props: Omit<Omit<DeleteProps, 'backend'>, 'entity'>): Promise<void> {
+
+  }
+
 }
--- interfaces/DbAdapter.ts
+++ interfaces/DbAdapter.ts
@@ -1,3 +1,16 @@
+import {CreateProps, DeleteProps, GetItemProps, UpdateProps} from '../frontend/interfaces/MethodsProps';
+import {GetResult} from '../frontend/interfaces/MethodsState';
+
 export default interface DbAdapter {
-  // TODO: add
+
+  // TODO: review
+
+  get(props: Omit<Omit<GetItemProps, 'backend'>, 'entity'>): Promise<GetResult>;
+
+  create(props: Omit<Omit<CreateProps, 'backend'>, 'entity'>): Promise<void>;
+
+  update(props: Omit<Omit<UpdateProps, 'backend'>, 'entity'>): Promise<void>;
+
+  delete(props: Omit<Omit<DeleteProps, 'backend'>, 'entity'>): Promise<void>;
+
 }
