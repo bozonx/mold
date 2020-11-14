aee3185cf497ea1904ad71ce9f4a2fb9671be694
ivan <ipkozyrin@gmal.com>
2020-11-14 15:41:30 +1000

    add

--- frontend/StateStorage.ts
+++ frontend/StateStorage.ts
@@ -3,6 +3,16 @@ import {ListState, ItemState} from './interfaces/MethodsState';
 
 
 export default class StateStorage {
+  // storage like { stateId: { loading: true, ... } }
+  private storage: {[index: string]: {[index: string]: any}} = {};
+
+
+  makeStateId(props: FindProps): string {
+    // TODO: use все виды props
+    // TODO: из параметров сделать уникальный id
+    // TODO: поля должны быть отсортированны
+  }
+
   setupList(props: FindProps, initialState: ListState<any>): string {
     // TODO: сгенерировать id запроса
     // TODO: если нет стейта то создать новый на основе initialState
@@ -15,6 +25,11 @@ export default class StateStorage {
   update(stateId: string, partialState: Partial<ListState<any>>) {
     // TODO: внести изменения
     // TODO: поднять события
+  }
+
+  destroy(stateId: string) {
+    // TODO: remove storage
+    // TODO: remove event listeners of storage
   }
 
   onChange(stateId, cb: (newState: any) => void): number {
