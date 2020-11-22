b7685308d7c6f138ff23a8219b20efe43a51f2c4
ivan <ipkozyrin@gmal.com>
2020-11-23 09:15:09 +1000

    add

---
+++ hooks/beforeAndAfterAction.ts
@@ -0,0 +1,33 @@
+import {MoldHook, PreHookDefinition, SetItem} from '../hooksMidleware/interfaces/MoldHook';
+import {handlePreHookDefinition} from '../hooksMidleware/hookHelpers';
+
+
+/**
+ * Position of hook is certainly that which is in the set.
+ */
+export function beforeAndAfterAction(
+  action: string,
+  hook: MoldHook | MoldHook[] | SetItem
+): SetItem {
+  if (!hook) throw new Error(`Please set almost one hook`);
+
+  if (Array.isArray(hook)) {
+    let result: PreHookDefinition[] = [];
+
+    for (let item of hook) {
+      result = [
+        ...result,
+        ...handlePreHookDefinition('before', item, [action]),
+        ...handlePreHookDefinition('after', item, [action]),
+      ]
+    }
+
+    return result;
+  }
+  else {
+    return [
+      ...handlePreHookDefinition('before', hook, [action]),
+      ...handlePreHookDefinition('after', hook, [action]),
+    ];
+  }
+}
