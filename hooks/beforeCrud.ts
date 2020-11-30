b4e9a0230788f9c0a6ab40d59b9e6d02074375ac
Ivan <ipkozyrin@gmail.com>
2020-11-30 11:56:30 +1000

    add

---
+++ hooks/beforeCrud.ts
@@ -0,0 +1,21 @@
+import {MoldHook, PreHookDefinition} from '../transform/interfaces/MoldHook';
+import {makeHooksDefinitions} from '../transform/hookHelpers';
+import {filterBlackList} from '../helpers/common';
+import {HOOK_CRUD_ACTIONS} from '../transform/interfaces/HookCrudActions';
+
+
+/**
+ * Position of hook is certainly that which is in the set.
+ * @param hook
+ * @param excludeCrudActions - if doesn't set then all the CRUD actions will be used.
+ */
+export function beforeCrud(
+  hook: MoldHook | MoldHook[],
+  excludeCrudActions?: string[]
+): PreHookDefinition[] {
+  if (!hook) throw new Error(`Please set almost one hook`);
+
+  const whiteList: string[] = filterBlackList(HOOK_CRUD_ACTIONS, excludeCrudActions);
+
+  return makeHooksDefinitions('before', hook, whiteList);
+}
