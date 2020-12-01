161c75cfb126dfc4074c0ea8d303196607ea2dc1
Ivan <ipkozyrin@gmail.com>
2020-12-01 12:07:48 +1000

    change

--- helpers/callDbAdapterAction.ts
+++ helpers/callDbAdapterAction.ts
@@ -1,9 +1,8 @@
 import {MoldRequest} from '../interfaces/MoldRequest';
 import {MoldResponse} from '../interfaces/MoldResponse';
 import {DbAdapter} from '../interfaces/DbAdapter';
+import {MoldDocument} from '../interfaces/MoldDocument';
 
-
-// TODO: review
 
 export function callDbAdapterAction(
   adapter: DbAdapter,
@@ -11,60 +10,64 @@ export function callDbAdapterAction(
 ): Promise<MoldResponse> {
   switch (request.action) {
     case 'find':
-
-      // TODO: наверное не нужнео ???
-      //if (!request.query) throw new Error(`No query in request ${JSON.stringify(request)}`)
-
-      return adapter.find(
-        request.set,
-        request.query || {}
-      );
+      return adapter.find(request.set,request.query || {});
     case 'get':
-      if (typeof request.id === 'undefined') throw new Error(
-        `No id in request ${JSON.stringify(request)}`
-      );
-
-      return adapter.get(
-        request.set,
-        request.id,
-        request.query,
-      );
+      return adapter.get(request.set, request.query || {});
     case 'create':
       if (!request.data) throw new Error(`No data in request ${JSON.stringify(request)}`);
 
-      return adapter.create(
-        request.set,
-        request.data,
-        request.query,
-      );
+      return adapter.create(request.set, request.data, request.query);
     case 'patch':
-      if (typeof request.id === 'undefined') throw new Error(
+      if (!request.data) throw new Error(`No data in request ${JSON.stringify(request)}`);
+
+      const doc = request.data as MoldDocument;
+
+      if (typeof doc.id === 'undefined') throw new Error(
         `No id in request ${JSON.stringify(request)}`
       );
-      else if (!request.data) throw new Error(`No data in request ${JSON.stringify(request)}`);
 
-      return adapter.patch(
-        request.set,
-        request.id,
-        request.data,
-        request.query,
-      );
+      return adapter.patch(request.set, doc, request.query);
     case 'delete':
       if (typeof request.id === 'undefined') throw new Error(
         `No id in request ${JSON.stringify(request)}`
       );
 
-      return adapter.delete(
+      return adapter.delete(request.set, request.id, request.query);
+    case 'batchCreate':
+      if (!request.data) throw new Error(`No data in request ${JSON.stringify(request)}`);
+      else if (!Array.isArray(request.data)) {
+        throw new Error(`Data has to be an array in request ${JSON.stringify(request)}`);
+      }
+
+      return adapter.batchCreate(
         request.set,
-        request.id,
-        request.query,
+        request.data as Partial<MoldDocument>[],
+        request.query
+      );
+    case 'batchPatch':
+      if (!request.data) throw new Error(`No data in request ${JSON.stringify(request)}`);
+      else if (!Array.isArray(request.data)) {
+        throw new Error(`Data has to be an array in request ${JSON.stringify(request)}`);
+      }
+
+      return adapter.batchPatch(
+        request.set,
+        request.data as MoldDocument[],
+        request.query
+      );
+    case 'batchDelete':
+      if (!request.data) throw new Error(`No data in request ${JSON.stringify(request)}`);
+      else if (!Array.isArray(request.data)) {
+        throw new Error(`Data has to be an array in request ${JSON.stringify(request)}`);
+      }
+
+      return adapter.batchDelete(
+        request.set,
+        request.data as (string | number)[],
+        request.query
       );
     default:
-
-      // TODO: call custom action
-
-      // throw new Error(
-      //   `Mold adapters doesn't support custom actions especially "${request.action}"`
-      // );
+      // call some custom action
+      return adapter.action(request.set, request.action, request.query, request.data);
   }
 }
