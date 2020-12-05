da220be8cf864cf0b5b1a396b41685131e607fb0
ivan <ipkozyrin@gmal.com>
2020-12-05 13:42:20 +1000

    change

--- interfaces/MoldSchema.ts
+++ interfaces/MoldSchema.ts
@@ -10,18 +10,21 @@ export type MoldSchemaFieldType = 'string'
 export interface MoldSchemaField {
   type: MoldSchemaFieldType;
   default?: JsonTypes;
-  unique?: boolean;
+  uniq?: boolean;
   required?: boolean;
   length?: number;
   relation?: string;
 }
 
-export interface MoldSchemaSet {
-  //set: string;
-  fields: Record<string, MoldSchemaField>;
-  seed?: Record<string, any>[];
+export interface MoldSchemaIdField {
+  type: 'string' | 'number',
+  length: number,
+  uniq: true,
 }
 
-export interface MoldSchema {
-  [index: string]: MoldSchemaSet;
+export interface MoldSchemaSet {
+  id?: MoldSchemaIdField;
+  [index: string]: MoldSchemaField | undefined;
 }
+// object like {setName: { fieldName: MoldSchemaField }}
+export type MoldSchema = Record<string, MoldSchemaSet>;
