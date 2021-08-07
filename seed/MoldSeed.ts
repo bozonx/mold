59dded321bfabffe7cc96dbda7644393c3bc4cc8
Ivan <ipkozyrin@gmail.com>
2021-08-07 12:12:58 +1000

    refactor

--- seed/MoldSeed.ts
+++ seed/MoldSeed.ts
@@ -5,82 +5,75 @@ import {MoldSeedContext} from '../interfaces/MoldSeedContext'
 import {DbAdapter} from '../interfaces/DbAdapter'
 import {SeedContext} from './SeedContext'
 import {MoldSchema} from '../interfaces/MoldSchema'
-//import {extractSeedFromSchema} from '../__old/extractSeedFromSchema';
-import {normalizeSchema} from '../schema/normalizeSchema';
 
 
-export default class MoldSeed {
-  private readonly adapter: DbAdapter;
-  private readonly seed: (context: MoldSeedContext) => void;
-  readonly log: Logger;
-  private readonly context: SeedContext;
+type SeedFunction = (context: MoldSeedContext) => void
 
-
-  constructor(
-    adapter: DbAdapter,
-    seed: (context: MoldSeedContext) => void,
-    rawSchema?: MoldSchema,
-    log?: Logger | LogLevel,
-  ) {
-
-    //const schema: MoldSchema = normalizeSchema(rawSchema);
-
-    // TODO: validate seed using schema
-
-    // TODO: remove
-    //this.seed = (seed) ? seed : extractSeedFromSchema(schemas!);
-    this.seed = seed
-    this.adapter = adapter
-    this.log = this.resolveLogger(log)
-    this.context = new SeedContext(this.adapter)
-
-    // TODO: запустить сразу
-  }
-
-  destroy() {
-    this.context.destroy();
-  }
-
-
-  start() {
-    this.startAsync()
-      .catch(this.log.error);
-  }
-
-  async startAsync() {
-    try {
-      this.seed(this.context);
-    }
-    catch (e) {
-      this.context.destroy();
-
-      throw e
-    }
-
-    await this.context.startActions();
-
-    this.log.info('Done');
-
-    this.context.destroy();
-  }
-
-  private resolveLogger(rawLogger?: Logger | LogLevel): Logger {
-    if (!rawLogger) return new ConsoleLogger();
-
-    if (typeof rawLogger === 'string') return new ConsoleLogger(rawLogger);
-
-    return rawLogger;
-  }
-
-}
-
-export function moldSeed(
+export function moldDoSeed(
   adapter: DbAdapter,
   seed: (context: MoldSeedContext) => void,
   schema?: MoldSchema,
   log: Logger | LogLevel = 'debug',
 ) {
-  const instance = new MoldSeed(adapter, seed, schema, log);
+  const instance = new MoldSeed(adapter, seed, schema, log)
 
-  instance.start();
+  instance.start()
+}
+
+
+export default class MoldSeed {
+  private readonly adapter: DbAdapter
+  private readonly seed: SeedFunction
+  readonly log: Logger
+  private readonly context: SeedContext
+
+
+  constructor(
+    adapter: DbAdapter,
+    seed: SeedFunction,
+    rawSchema?: MoldSchema,
+    log?: Logger | LogLevel,
+  ) {
+    this.seed = seed
+    this.adapter = adapter
+    this.log = this.resolveLogger(log)
+    this.context = new SeedContext(this.adapter, rawSchema)
+  }
+
+  destroy() {
+    this.context.destroy()
+  }
+
+
+  start() {
+    this.startAsync()
+      .catch(this.log.error)
+  }
+
+  async startAsync() {
+    try {
+      this.seed(this.context)
+    }
+    catch (e) {
+      this.context.destroy()
+
+      throw e
+    }
+
+    await this.context.startActions()
+
+    this.log.info('Done')
+
+    this.context.destroy()
+  }
+
+
+  private resolveLogger(rawLogger?: Logger | LogLevel): Logger {
+    if (!rawLogger) return new ConsoleLogger()
+
+    if (typeof rawLogger === 'string') return new ConsoleLogger(rawLogger)
+
+    return rawLogger
+  }
+
 }
--- seed/SeedContext.ts
+++ seed/SeedContext.ts
@@ -1,37 +1,50 @@
-import {MoldSeedContext} from '../interfaces/MoldSeedContext';
-import {DbAdapter} from '../interfaces/DbAdapter';
-import {stringifyMoldError} from '../helpers/helpers';
+import {MoldSeedContext} from '../interfaces/MoldSeedContext'
+import {DbAdapter} from '../interfaces/DbAdapter'
+import {stringifyMoldError} from '../helpers/helpers'
+import {MoldSchema} from '../interfaces/MoldSchema'
+import {normalizeSchema} from '../schema/normalizeSchema'
 
 
 export class SeedContext implements MoldSeedContext {
-  private readonly adapter: DbAdapter;
-  private actions: (() => void)[] = [];
+  private readonly adapter: DbAdapter
+  private readonly schema: MoldSchema | undefined
+  private actions: (() => void)[] = []
 
 
-  constructor(adapter: DbAdapter) {
-    this.adapter = adapter;
+  constructor(adapter: DbAdapter, rawSchema?: MoldSchema,) {
+    this.adapter = adapter
+    this.schema = rawSchema && normalizeSchema(rawSchema)
   }
 
   destroy() {
-    this.actions = [];
+    this.actions = []
   }
 
 
   insert(set: string, docs: Record<string, any>[]) {
     this.actions.push(async () => {
-      const result = await this.adapter.batchCreate(set, docs);
+      for (const doc of docs) {
+        this.validateDoc(set, doc)
+      }
 
-      if (result.success) return result;
+      const result = await this.adapter.batchCreate(set, docs)
+
+      if (result.success) return result
 
       // TODO: оно вообще так будет ???
-      throw new Error(stringifyMoldError(result.errors));
+      throw new Error(stringifyMoldError(result.errors))
     });
   }
 
   async startActions() {
     for (let cb of this.actions) {
-      await cb();
+      await cb()
     }
+  }
+
+
+  private validateDoc(set: string, doc: Record<string, any>) {
+    // TODO: validate seed using schema
   }
 
 }
